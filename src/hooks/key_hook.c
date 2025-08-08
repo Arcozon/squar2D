940cd7c6e1808f08eee022472b0ac2354eb26ffa
Gabriel Eudes <gaeudes@student.42.fr>
2025-08-08 19:46:56 +0200

    [PARS MAP] map is read, i dectect wrong chars, need to normalise it (same size line), check nb player and if its closed

--- src/hooks/key_hook.c
+++ src/hooks/key_hook.c
@@ -6,7 +6,7 @@
 /*   By: gaeudes <gaeudes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 15:40:04 by gaeudes           #+#    #+#             */
-/*   Updated: 2025/08/08 16:52:21 by gaeudes          ###   ########.fr       */
+/*   Updated: 2025/08/08 19:44:37 by gaeudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,22 @@ int	key_press_hook(int key, t_cub *cub)
 {
 	if (key == XK_Escape)
 		mlx_loop_end(cub->pars.pmlx.mlx_ptr);
-	(void)key, (void)cub;
+	else if (key == XK_space)
+	{
+		t_vector vmap = cub->pars.vec_map;
+		for (uint64_t i = 0; i < vmap.size; ++i)
+		{
+			printf("%.*s\n", (int)vmap.u_ptr.vect_ptr[i].size,  vmap.u_ptr.vect_ptr[i].u_ptr.char_ptr);
+		}
+	}
 	return (0);
+	(void)key, (void)cub;
 }
 
 int		key_release_hook(int key, t_cub *cub)
 {
 	if (key == XK_Escape)
 		mlx_loop_end(cub->pars.pmlx.mlx_ptr);
-	(void)key, (void)cub;
 	return (0);
+	(void)key, (void)cub;
 }
