49de9ce1d3640cd405d70b9ed7c17857d07c611d
Gabriel Eudes <gaeudes@student.42.fr>
2025-08-23 19:00:53 +0200

    [HOOKS] I know wich keys are pressed at a specific time

--- src/hooks/key_hook.c
+++ src/hooks/key_hook.c
@@ -6,7 +6,7 @@
 /*   By: gaeudes <gaeudes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 15:40:04 by gaeudes           #+#    #+#             */
-/*   Updated: 2025/08/19 16:15:55 by gaeudes          ###   ########.fr       */
+/*   Updated: 2025/08/23 18:54:24 by gaeudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,38 @@ void	print_map(t_game *game)
 	printf("[%f|%f] [%f]\n", (float)game->p_coo[X], (float)game->p_coo[Y], game->p_angle);
 }
 
+void	handle_press(const int key, uint64_t *f_key)
+{
+	if (key == XK_w || key == XK_W)
+		*f_key |= HOOK_M_W;
+	else if (key == XK_a || key == XK_A)
+		*f_key |= HOOK_M_A;
+	else if (key == XK_s || key == XK_S)
+		*f_key |= HOOK_M_S;
+	else if (key == XK_d || key == XK_D)
+		*f_key |= HOOK_M_D;
+	else if (key == XK_Right)
+		*f_key |= HOOK_M_RARR;
+	else if (key == XK_Left)
+		*f_key |= HOOK_M_LARR;
+}
+
+void	handle_release(const int key, uint64_t *f_key)
+{
+	if (key == XK_w || key == XK_W)
+		*f_key &= ~HOOK_M_W;
+	else if (key == XK_a || key == XK_A)
+		*f_key &= ~HOOK_M_A;
+	else if (key == XK_s || key == XK_S)
+		*f_key &= ~HOOK_M_S;
+	else if (key == XK_d || key == XK_D)
+		*f_key &= ~HOOK_M_D;
+	else if (key == XK_Right)
+		*f_key &= ~HOOK_M_RARR;
+	else if (key == XK_Left)
+		*f_key &= ~HOOK_M_LARR;
+}
+
 int	key_press_hook(int key, t_game *game)
 {
 	if (key == XK_Escape)
@@ -27,6 +59,8 @@ int	key_press_hook(int key, t_game *game)
 		print_map(game);
 	else if (key == XK_space)
 		draw_frame(game);
+	else
+		handle_press(key, &game->f_keys);
 	return (0);
 }
 
@@ -34,5 +68,6 @@ int	key_release_hook(int key, t_game *game)
 {
 	if (key == XK_Escape)
 		mlx_loop_end(game->render.pmlx);
+	handle_release(key, &game->f_keys);	
 	return (0);
 }
--- src/hooks/loop.c
+++ src/hooks/loop.c
@@ -6,14 +6,35 @@
 /*   By: gaeudes <gaeudes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 16:50:42 by gaeudes           #+#    #+#             */
-/*   Updated: 2025/08/19 16:16:11 by gaeudes          ###   ########.fr       */
+/*   Updated: 2025/08/23 19:00:13 by gaeudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	debug_key_pressed(t_game *game)
+{
+	static uint64_t		last_press;
+	static const char	key_c[] = "WASDLR";
+
+	if (last_press != game->f_keys)
+	{
+		last_press = game->f_keys;
+		if (last_press)
+		{
+			for(int i = 0; key_c[i]; ++i)
+				if (last_press & (1 << i))
+					fprintf(stderr, "%c", key_c[i]);
+		}
+		else
+			fprintf(stderr, "none");
+		fprintf(stderr, "\n");
+	}
+}
+
 int	cub_loop(t_game *game)
 {
+	debug_key_pressed(game);	
 	// draw_frame(game);
 	return (0);
 	(void)game;
