4567f11b6cc0783b5c5437072c04693b5c0891e9
Gabriel Eudes <gaeudes@student.42.fr>
2025-08-27 17:08:19 +0200

    [DEBUG_H] Timer added

--- inc/utils/debug.h
+++ inc/utils/debug.h
@@ -6,7 +6,7 @@
 /*   By: gaeudes <gaeudes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 15:09:09 by gaeudes           #+#    #+#             */
-/*   Updated: 2025/08/25 15:45:27 by gaeudes          ###   ########.fr       */
+/*   Updated: 2025/08/27 17:07:42 by gaeudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,104 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <string.h>
+# include <time.h>
 
 # define RESET		"[0m"
 # define B_BLUE		"[1;34m"
 # define BLUE		"[34m"
+# define RED		"[31m"
+# define B_RED		"[1;31m"
 # define GREY		"[30m"
 # define GREEN		"[32m"
+# define B_GREEN	"[1;32m"
 # define B_LYELLOW	"[1;93m"
 # define B_MAJENTA	"[1;35m"
 # define B_WHITE	"[1;37m"
 
-# define BASENAME(string)		(char *)(strrchr(string, '/') ? strrchr(string, '/') + 1 : (char *)string)
+# define DEBUG_ON
 
-# define DEBUG_PROMPT		B_BLUE "%s" GREY "[" GREEN "%d" GREY "]" RESET ":"
-# define DEBUG_PROMPT_ARGS	BASENAME(__FILE__), __LINE__ 
+# ifdef DEBUG_ON
+
+# define BASENAME(string)		(char *)(strrchr(string, '/') ? strrchr(string, '/') + 1 : (char *)string)
 
 # define WAIT_PROMPT		B_MAJENTA "WAIT" B_LYELLOW "[" RESET BLUE "%s" GREY "(" GREEN "%d" GREY ")"  B_LYELLOW "]" RESET " "
 # define WAIT_PROMPT_ARGS	BASENAME(__FILE__), __LINE__
 
-# define DEBUG_ON
-
-# ifdef DEBUG_ON
 #  define WAIT\
 	{\
 		char	g = 0;\
 		fprintf(stderr, WAIT_PROMPT, WAIT_PROMPT_ARGS);\
 		while (read(0, &g, 1) == 1 && g != '\n') ;\
 	}
+
+
+# define DEBUG_PROMPT		B_BLUE "%s" GREY "[" GREEN "%d" GREY "]" RESET ":"
+# define DEBUG_PROMPT_ARGS	BASENAME(__FILE__), __LINE__ 
+
 #  define DEBUG(s, ...)\
 	{\
 			fprintf(stderr, DEBUG_PROMPT" "s"\n", DEBUG_PROMPT_ARGS, ##__VA_ARGS__);\
 	}
 
+# define DEBUG_TIMER_BUFFER_SIZE	1024
+
+# ifdef DEBUG_INIT
+	double	g_debug_time;
+	char	g_debug_start_file[DEBUG_TIMER_BUFFER_SIZE];
+	int		g_debug_start_line;
+	
+	static void	__attribute__((constructor))	debug_init_timer(void) {	g_debug_time = 0;	};
+# else
+	extern double	g_debug_time;
+	extern char		g_debug_start_file[DEBUG_TIMER_BUFFER_SIZE];
+	extern int		g_debug_start_line;
+# endif
+
+__attribute__((unused, always_inline, const, pure))
+static inline double	debug_clock(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (ts.tv_sec + ts.tv_nsec / 1e9);
+} 
+
+#  define TIMER_START\
+	{\
+		g_debug_time = debug_clock();\
+		strncpy(g_debug_start_file, BASENAME(__FILE__), (sizeof(g_debug_start_file) - 1));\
+		g_debug_start_file[(sizeof(g_debug_start_file) - 1)] = 0;\
+		g_debug_start_line = __LINE__;\
+	};
+
+
+# define DEBUG_TIMER_FILEFORMAT	RESET BLUE "%s" GREY "(" BLUE "%d" GREY")" RESET
+# define DEBUG_TIMER_FILEARGS(MACRO_FILE, MACRO_LINE)		 BASENAME(MACRO_FILE), MACRO_LINE
+
+# define DEBUG_END_TIMER_FORMAT	B_GREEN "Timer" RESET GREY "["\
+			DEBUG_TIMER_FILEFORMAT GREY "-"\
+			DEBUG_TIMER_FILEFORMAT GREY "]: " GREEN "%lfs\n" RESET
+
+# define DEBUG_END_TIMER_ARGS	DEBUG_TIMER_FILEARGS(g_debug_start_file, g_debug_start_line),\
+								DEBUG_TIMER_FILEARGS(__FILE__, __LINE__),\
+								((debug_clock() - g_debug_time))
+
+#  define TIMER_END\
+	{\
+		if (g_debug_time == 0)\
+		{\
+			fprintf(stderr, B_RED "Timer never started (%s[%d]\n", __FILE__, __LINE__);\
+		}\
+		else\
+		{\
+			fprintf(stderr, DEBUG_END_TIMER_FORMAT, DEBUG_END_TIMER_ARGS);\
+			g_debug_time = 0;\
+		}\
+	};
+
 # else
 #  define WAIT			{}
 #  define DEBUG(s , ...)	{(void)s;}
-
+#  define TIMER_START
+#  define TIMER_END
 # endif
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,18 +6,23 @@
 /*   By: gaeudes <gaeudes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 16:25:42 by gaeudes           #+#    #+#             */
-/*   Updated: 2025/08/27 12:47:41 by gaeudes          ###   ########.fr       */
+/*   Updated: 2025/08/27 16:58:51 by gaeudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+#define DEBUG_INIT
 
 #include "cub3d.h"
 
 int	main(int ac, char *av[])
 {
+	TIMER_START
 	__attribute__((cleanup(free_cub))) t_cub cub;
 	if (!init_cub(&cub, ac, av))
 	{
 		mlx_loop(cub.pars.pmlx.mlx_ptr);
 	}
+	TIMER_END
 	return (perror_cub(cub));
 }
