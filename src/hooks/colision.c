3f2b74c7f3343fb851629b6e853ee967b353b6e4
Gabriel Eudes <gaeudes@student.42.fr>
2025-09-09 15:20:37 +0200

    [COLISION] Fixed, looking in the 4 corners

--- src/hooks/colision.c
+++ src/hooks/colision.c
@@ -6,120 +6,46 @@
 /*   By: gaeudes <gaeudes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 14:50:55 by gaeudes           #+#    #+#             */
-/*   Updated: 2025/09/08 17:55:10 by gaeudes          ###   ########.fr       */
+/*   Updated: 2025/09/09 15:20:22 by gaeudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+__attribute__((always_inline))
+static inline void	__check_one_col(char *map[], const float coo[2], float p_delta[2])
+{
+	const int	to_add[2] = {(p_delta[X] < 0), (p_delta[Y] < 0)};
+	const float	n_coo[2] = {coo[X] + p_delta[X], coo[Y] + p_delta[Y]};
+	const int	crossed[2] = {(int)coo[X] != (int)n_coo[X], (int)coo[Y] != (int)n_coo[Y]};
 
-// __attribute__((always_inline))
-// void	init_one_col(const float start[2], const float p_delta[2], t_col *col,
-// 	const int offset[2])
-// {
-// 	col->start_coo[X] = start[X] + offset[X] * HALF_P_SIZE;
-// 	col->start_coo[Y] = start[Y] + offset[Y] * HALF_P_SIZE;
-// 	col->coo[X] = col->start_coo[X];
-// 	col->coo[Y] = col->start_coo[Y];
-// 	col->p_delta[X] = p_delta[X];
-// 	col->p_delta[Y] = p_delta[Y];
-// 	col->tt_nstep[X] = 0.f;
-// 	col->tt_nstep[Y] = 0.f;
-// 	col->to_add[X] = 0;
-// 	col->dir[X] = -1;
-// 	if (p_delta[X] > 0)
-// 	{
-// 		col->dir[X] = 1;
-// 		col->to_add[X] = 1;
-// 	}
-// 	col->to_add[Y] = 0;
-// 	col->dir[Y] = 1;
-// 	if (p_delta[Y] < 0)
-// 	{
-// 		col->dir[Y] = -1;
-// 		col->to_add[Y] = 1;
-// 	}
-// 	col->hit = 0;
-// 	col->done = 0;
-// 	col->ratio2[X] = 0;
-// 	if (col->p_delta[Y] < -ZERO_RANGE || col->p_delta[Y] > ZERO_RANGE)
-// 		col->ratio2[X] = p_delta[X] / p_delta[Y];
-// 	col->ratio2[X] *= col->ratio2[X];
-// 	col->ratio2[Y] = 0;
-// 	if (col->p_delta[X] < -ZERO_RANGE || col->p_delta[X] > ZERO_RANGE)
-// 		col->ratio2[Y] = p_delta[Y] / p_delta[X];
-// 	col->ratio2[Y] *= col->ratio2[Y];
-// }
-
-// __attribute__((always_inline))
-// int	check_one_colision(t_col *col, char *map[])
-// {
-// 	DEBUG("COO %f %f", col->coo[X], col->coo[Y])
-// 	while (!col->done && (!col->hit[X] || !col->hit[Y]))
-// 	{
-// 		col->n_step[X] = ((int)col->coo[X] + col->to_add[X]) - col->coo[X];
-// 		if (col->n_step[X] == 0.f)
-// 			col->n_step[X] = col->dir[X];
-// 		col->n_step[Y] = ((int)col->coo[Y] + col->to_add[Y]) - col->coo[Y];
-// 		if (col->n_step[Y] == 0)
-// 			col->n_step[Y] = col->dir[Y];
-// 		col->dist_n_step_2[X] = col->n_step[X] * col->n_step[X] * (1 + col->ratio2[Y]);
-// 		DEBUG("rX2 %f", col->ratio2[X])
-// 		DEBUG("sX: %f, dX %f", col->n_step[X], col->dist_n_step_2[X])
-// 		col->dist_n_step_2[Y] = col->n_step[Y] * col->n_step[Y] * (1 + col->ratio2[X]);
-// 		DEBUG("rY2 %f", col->ratio2[Y])
-// 		DEBUG("sY: %f, dY %f", col->n_step[Y], col->dist_n_step_2[Y])
-// 		WAIT
-// 		// DEBUG("X[%f] [%f|%d] %f", col->p_delta[X], col->coo[X], (int)col->coo[X] + col->to_add[X], col->n_step[X])
-// 		// DEBUG("Y[%f] [%f|%d] %f", col->p_delta[Y], col->coo[Y], (int)col->coo[Y] + col->to_add[Y], col->n_step[Y])
-// 		col->done = 1;
-// 		if (col->dist_n_step_2[X] <= col->dist_n_step_2[Y] && fabs(col->tt_nstep[X]) + fabs(col->n_step[X]) <= fabs(col->p_delta[X]))
-// 		{
-// 			col->done = 0;
-// 			DEBUG("Xcheck %d %d", (int)col->coo[X] + col->dir[X], (int)col->coo[Y])
-// 			if (map[(int)col->coo[Y]][(int)col->coo[X] + col->dir[X]] == WALL_CHAR)
-// 			{
-// 				DEBUG("Xcol")
-// 				col->hit[X] = 1;
-// 			}
-// 			col->tt_nstep[X] += col->n_step[X];
-// 			col->coo[X] += col->n_step[X];
-// 			DEBUG("COO %f %f", col->coo[X], col->coo[Y])
-// 		}
-// 		else if (fabs(col->tt_nstep[Y]) + fabs(col->n_step[Y]) <= fabs(col->p_delta[Y]))
-// 		{
-// 			col->done = 0;
-// 			DEBUG("Ycheck %d %d", (int)col->coo[X], (int)col->coo[Y] + col->dir[Y])
-// 			if (map[(int)col->coo[Y] + col->dir[Y]][(int)col->coo[X]] == WALL_CHAR)
-// 			{
-// 				DEBUG("Ycol")
-// 				col->hit[Y] = 1;
-// 			}
-// 			col->tt_nstep[Y] += col->n_step[Y];
-// 			col->coo[Y] += col->n_step[Y];
-// 		}
-// 		WAIT
-// 	}
-// 	DEBUG("DONE")
-// 	return (1);
-// 	(void)map;
-// }
+	if (crossed[X] && crossed[Y] && 1)
+	{
+		; // check ?
+	}
+	if (crossed[X] && map[(int)coo[Y]][(int)n_coo[X]] == WALL_CHAR)
+	{
+		p_delta[X] = ((int)n_coo[X] + to_add[X]) - coo[X];
+		if (!to_add[X])
+			p_delta[X] -= ZERO_RANGE;
+	}
+	if (crossed[Y] && map[(int)n_coo[Y]][(int)coo[X]] == WALL_CHAR)
+	{
+		p_delta[Y] = ((int)n_coo[Y] + to_add[Y]) - coo[Y];
+		if (!to_add[Y])
+			p_delta[Y] -= ZERO_RANGE;
+	}
+}
 
 __attribute__((flatten))
 void	check_colisions(const float p_coo[2], float p_delta[2], char *map[])
 {
-	// t_col	col;
-
-	// p_delta[X] = 3;
-	// p_delta[Y] = 2;
-	// // init_one_col(p_coo, p_delta, &col, (int []){1, 1});
-	// init_one_col(p_coo, p_delta, &col, (int []){0, 0});
-	// if (check_one_colision(&col, map))
-	// {
-	// 	p_delta[X] = col.start_coo[X] - col.coo[X];
-	// }
-	// init_one_col(p_coo, p_delta, &col, (int []){1, -1});
-	// init_one_col(p_coo, p_delta, &col, (int []){-1, 1});
-	// init_one_col(p_coo, p_delta, &col, (int []){-1, -1});
-	(void)p_coo, (void)p_delta, (void)map;
+	const float	coo_nw[2] = {p_coo[X] - HALF_P_SIZE, p_coo[Y] - HALF_P_SIZE};
+	const float	coo_ne[2] = {p_coo[X] + HALF_P_SIZE, p_coo[Y] - HALF_P_SIZE};
+	const float	coo_se[2] = {p_coo[X] + HALF_P_SIZE, p_coo[Y] + HALF_P_SIZE};
+	const float	coo_sw[2] = {p_coo[X] - HALF_P_SIZE, p_coo[Y] + HALF_P_SIZE};
+	__check_one_col(map, coo_nw, p_delta);
+	__check_one_col(map, coo_ne, p_delta);
+	__check_one_col(map, coo_se, p_delta);
+	__check_one_col(map, coo_sw, p_delta);
 }
--- src/hooks/move_player.c
+++ src/hooks/move_player.c
@@ -6,7 +6,7 @@
 /*   By: gaeudes <gaeudes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 16:06:20 by gaeudes           #+#    #+#             */
-/*   Updated: 2025/08/28 18:30:00 by gaeudes          ###   ########.fr       */
+/*   Updated: 2025/09/09 14:50:48 by gaeudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ static inline void	handle_view_angle(t_game *game)
 
 static inline void	handle_2_dir(float p_delta[2], const float p_speed)
 {
+	// float	norm_factor = (fabs(p_delta[X]) + fabs(p_delta[Y])) / p_speed;	// check for pitagor instead of add
 	float	norm_factor = (fabs(p_delta[X]) + fabs(p_delta[Y])) / p_speed;
 
 	p_delta[X] /= norm_factor;
@@ -57,54 +58,14 @@ static inline void	get_new_delta(const t_game *game, float p_delta[2])
 		handle_2_dir(p_delta, game->p_speed);
 }
 
-static inline void	calc_colision(char *map[], const float p_coo[2], float p_delta[2])
-{
-	const float	next_pcoo[2] = {p_coo[X] + p_delta[X], p_coo[Y] + p_delta[Y]};
 
-	if (map[(int)p_coo[Y]][(int)(next_pcoo[X] + HALF_P_SIZE)] == WALL_CHAR)
-	{
-		DEBUG("COL right")
-		DEBUG("WALL: %d", (int)(next_pcoo[X] + HALF_P_SIZE))
-		DEBUG("Pl: %f", p_coo[X])
-		DEBUG("Pre: %f", p_delta[X])
-		p_delta[X] = (int)(next_pcoo[X] + HALF_P_SIZE) - HALF_P_SIZE - p_coo[X];
-		DEBUG("Post: %f", p_delta[X])
-	}
-	else if (map[(int)p_coo[Y]][(int)(next_pcoo[X] - HALF_P_SIZE)] == WALL_CHAR)
-	{
-		DEBUG("COL left")
-		DEBUG("WALL: %d", (int)(next_pcoo[X] - HALF_P_SIZE))
-		DEBUG("Pl: %f", p_coo[X])
-		DEBUG("Pre: %f", p_delta[X])
-		p_delta[X] = (int)(next_pcoo[X] + 1 - HALF_P_SIZE) + HALF_P_SIZE - p_coo[X];
-		DEBUG("Post: %f", p_delta[X])
-	}
-	if (map[(int)(next_pcoo[Y] + HALF_P_SIZE)][(int)(p_coo[X])] == WALL_CHAR)
-	{
-		DEBUG("COL bot")
-		DEBUG("WALL: %d", (int)(next_pcoo[Y] + HALF_P_SIZE))
-		DEBUG("Pl: %f", p_coo[Y])
-		DEBUG("Pre: %f", p_delta[Y])
-		p_delta[Y] = (int)(next_pcoo[Y] + HALF_P_SIZE) - HALF_P_SIZE - p_coo[Y];
-		DEBUG("Post: %f", p_delta[Y])
-	}
-	else if (map[(int)(next_pcoo[Y] - HALF_P_SIZE)][(int)(p_coo[X])] == WALL_CHAR)
-	{
-		DEBUG("COL top")
-		DEBUG("WALL: %d", (int)(next_pcoo[Y] - HALF_P_SIZE))
-		DEBUG("Pl: %f", p_coo[Y])
-		DEBUG("Pre: %f", p_delta[Y])
-		p_delta[Y] = (int)(next_pcoo[Y] + 1 - HALF_P_SIZE) + HALF_P_SIZE - p_coo[Y];
-		DEBUG("Post: %f", p_delta[Y])
-	}
-}
 
 __attribute__((flatten))
 void	move_player(t_game *game)
 {
 	handle_view_angle(game);
 	get_new_delta(game, game->p_delta);
-	calc_colision(game->map, game->p_coo, game->p_delta);
+	check_colisions(game->p_coo, game->p_delta, game->map);
 	game->p_coo[X] += game->p_delta[X];
 	game->p_coo[Y] += game->p_delta[Y];
 	ft_bzero(game->p_delta, sizeof(game->p_delta));
